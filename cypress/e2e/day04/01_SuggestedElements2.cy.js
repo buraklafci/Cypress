fc298202a1ee86360813d5492533b341cb374ea2
burak <karanfilkaranfil822@gmail.com>
2023-05-02 18:39:26 +0300

    2.5.23

--- cypress/e2e/day04/01_SuggestedElements2.cy.js
+++ cypress/e2e/day04/01_SuggestedElements2.cy.js
@@ -6,7 +6,7 @@ describe ('Suggested Elements',()=>{
         cy.get('#twotabsearchtextbox').type('dress')
         cy.get(':nth-child(4) > .s-suggestion-container > .s-suggestion').contains('dress').click()
         cy.wait(3000)
-        cy.get('[data-asin="B08LL9CPXD"] > .sg-col-inner > .s-widget-container > .s-card-container > .a-spacing-base > .s-product-image-container > .rush-component > .a-link-normal > .a-section > .s-image').click()
+        cy.get('[data-asin="B09NF6C17V"] > .sg-col-inner > .s-widget-container > .s-card-container > .a-spacing-base > .a-spacing-small > .a-spacing-top-small > .a-size-mini > .a-link-normal > .a-size-base-plus').click()
         cy.wait(3000)
         cy.get('#productTitle').should('be.visible')
     })
